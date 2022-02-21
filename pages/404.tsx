fcff7b888227bd51d11d75a079b47c436fba49b1
wwwhoami <subman450@gmail.com>
2022-02-21 19:57:43 +0300

    404 page fix

--- pages/404.tsx
+++ pages/404.tsx
@@ -9,13 +9,7 @@ const NotFoundPage: NextPage<Props> = (props) => {
   return (
     <Layout title="Page Not Found">
       <div className="flex flex-col items-center mt-20">
-        <Image
-          src="/images/logo.png"
-          alt="logo"
-          width={70}
-          height={70}
-          className="bg-gray-900 rounded-xl"
-        />
+        <Image src="/images/logo.png" alt="logo" width={140} height={70} />
         <h1 className="text-6xl my-5">Whoops</h1>
         <h2 className="text-4xl text-gray-400">This page does not exist :(</h2>
       </div>
