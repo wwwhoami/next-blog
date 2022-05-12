d14be4081590de208999fa427eae7d72acf97b84
wwwhoami <subman450@gmail.com>
2022-05-12 17:03:07 +0300

    PostService.getPostsFrontmatter reimplemented

--- services/PostService.ts
+++ services/PostService.ts
@@ -14,41 +14,54 @@ type Post = {
   category: string
   hexColor: string
 }
+type GetPostsQueryParams = {
+  take?: number
+  skip?: number
+  order?: order
+  orderBy?: string
+}
 
-async function getPostsFrontmatter(
-  limit: number = 10,
-  offset: number = 0,
-  order: order = 'desc',
-  orderBy: string = 'createdAt'
-) {
+export async function getPostsFrontmatter({
+  take = 10,
+  skip = 0,
+  order = 'desc',
+  orderBy = 'createdAt',
+}: GetPostsQueryParams) {
   try {
-    const posts = await prisma.$queryRaw<Post[]>`
-    SELECT
-      P.created_at as "createdAt",
-      P.title,
-      P.slug,
-      P.excerpt,
-      P.view_count AS "viewCount",
-      P.cover_image AS "coverImage",
-      U.name AS "authorName",
-      U.image AS "authorImage",
-      PC.name AS category,
-      PC.hex_color AS "hexColor"
-    FROM
-      "Post" AS P
-    JOIN "User" AS U ON U.id = P.author_id
-    JOIN (
-      SELECT
-          PTC.post_id,
-          C.name,
-          C.hex_color
-      FROM
-          "PostToCategory" AS PTC
-      JOIN "Category" AS C ON C.name = PTC.category_name
-    ) AS PC ON PC.post_id = P.id
-    WHERE P.published = TRUE
-    ORDER BY ${orderBy} ${order} 
-    LIMIT ${limit} OFFSET ${offset}`
+    const posts = await prisma.post.findMany({
+      select: {
+        createdAt: true,
+        title: true,
+        slug: true,
+        excerpt: true,
+        viewCount: true,
+        coverImage: true,
+        author: {
+          select: {
+            name: true,
+            image: true,
+          },
+        },
+        categories: {
+          select: {
+            category: {
+              select: {
+                name: true,
+                hexColor: true,
+              },
+            },
+          },
+        },
+      },
+      where: {
+        published: true,
+      },
+      orderBy: {
+        [orderBy]: order,
+      },
+      skip,
+      take,
+    })
 
     return posts
   } catch (error) {
@@ -59,7 +72,7 @@ async function getPostsFrontmatter(
   }
 }
 
-async function getPostBySlug(slug: string) {
+export async function getPostBySlug(slug: string) {
   try {
     const post = await prisma.post.findFirst({
       select: {
