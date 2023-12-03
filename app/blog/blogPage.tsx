b16584fdafd41f453426c9996b0a7cca19332860
wwwhoami <subman450@gmail.com>
2023-12-03 16:48:46 +0300

    blogPage: fetch "revalidate: false"

--- app/blog/blogPage.tsx
+++ app/blog/blogPage.tsx
@@ -28,6 +28,10 @@ type Props = {
   fallbackData: Post[][]
 }
 
+function blogPageFetcher(url: string) {
+  return fetcher<Post[]>(url, { cache: 'no-store' })
+}
+
 export default function BlogPage({ fallbackData }: Props) {
   const searchParams = useSearchParams()
 
@@ -36,7 +40,7 @@ export default function BlogPage({ fallbackData }: Props) {
 
   const { ref, data, isEmpty } = useInfiniteLoading(
     (...args) => getKey(...args, searchQuery, category),
-    fetcher,
+    blogPageFetcher,
     fallbackData,
   )
 
@@ -44,15 +48,15 @@ export default function BlogPage({ fallbackData }: Props) {
 
   return (
     <>
-      <h1 className="border-b-4 p-3 text-4xl font-semibold">Posts</h1>
+      <h1 className="p-3 text-4xl font-semibold border-b-4">Posts</h1>
       <CategorySelect />
       {isEmpty && (
-        <div className="mt-20 flex flex-col items-center">
+        <div className="flex flex-col items-center mt-20">
           <h1 className="my-5 text-6xl">Whoops</h1>
           <h2 className="text-4xl text-gray-400">No posts found :(</h2>
         </div>
       )}
-      <div className="my-3 grid gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid gap-5 my-3 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {posts?.map((post) => <PostCard key={post.id} post={post} />)}
       </div>
       <div ref={ref}></div>
