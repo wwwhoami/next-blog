bdad081d6508086649971a68942badebd65ed3ce
wwwhoami <subman450@gmail.com>
2023-12-14 19:15:36 +0300

    not-found: png img -> svg

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,15 @@
+import { ExclamationTriangleIcon } from '@heroicons/react/24/solid'
 import { NextPage } from 'next'
-import Image from 'next/image'
 
 type Props = {}
 
 const NotFoundPage: NextPage<Props> = (props) => {
   return (
     <div className="flex flex-col items-center mt-20">
-      <Image src="/images/logo.png" alt="logo" width={140} height={70} />
-      <h1 className="my-5 text-6xl dark:text-white">404</h1>
+      <h1 className="my-5 text-6xl dark:text-white">
+        <ExclamationTriangleIcon className="inline-block mx-2 w-14 h-14" />
+        404
+      </h1>
       <h2 className="text-4xl text-gray-400 dark:text-slate-200">
         Whoops... Page does not exist :(
       </h2>
