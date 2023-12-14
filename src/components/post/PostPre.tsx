5a9b6a5f743692e2fdeacb5bc8ce552a705d1012
wwwhoami <subman450@gmail.com>
2023-12-14 18:43:56 +0300

    PostPre: copy button content changes onCopy

--- src/components/post/PostPre.tsx
+++ src/components/post/PostPre.tsx
@@ -2,7 +2,7 @@
 
 import { useTheme } from '@/context/ThemeProvider'
 import { ClipboardDocumentIcon } from '@heroicons/react/20/solid'
-import React from 'react'
+import React, { useState } from 'react'
 import { toast } from 'react-toastify'
 
 type Props = {
@@ -14,16 +14,21 @@ type Props = {
 
 export default function PostPre(props: Props) {
   const { isDarkTheme } = useTheme()
+  const [copied, setCopied] = useState(false)
   const currentTheme = isDarkTheme ? 'dark' : 'light'
 
   const copyToClipboard = async () => {
     await navigator.clipboard.writeText(props.raw)
+    setCopied(true)
+
     toast.success(
       <p>
         Copied to clipboard
         <ClipboardDocumentIcon className="inline-block w-5 h-5 ml-2" />
       </p>,
     )
+
+    setTimeout(() => setCopied(false), 5000)
   }
 
   return (
@@ -33,10 +38,13 @@ export default function PostPre(props: Props) {
         <div className="flex items-center justify-between w-full pb-3 text-sm text-gray-600 dark:text-gray-300">
           <span>{props['data-language']}</span>
           <button
-            className="p-2 rounded-lg focus-ring hover:bg-gray-200 hover:text-gray-900 dark:hover:text-gray-900"
+            className={`p-2 rounded-lg focus-ring hover:bg-gray-200 hover:text-gray-900 dark:hover:text-gray-900
+             focus:bg-gray-200 focus:text-gray-900 dark:focus:text-gray-900
+             disabled:bg-opacity-70 disabled:cursor-not-allowed`}
             onClick={copyToClipboard}
+            disabled={copied}
           >
-            Copy
+            {copied ? 'Copied!' : 'Copy'}
           </button>
         </div>
         {props.children}
