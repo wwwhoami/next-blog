f8112a10d28033c937065918b2fffbb2ab3591d0
wwwhoami <subman450@gmail.com>
2023-12-20 20:31:38 +0300

    Add validatePassword utility function

---
+++ src/utils/validatePassword.ts
@@ -0,0 +1,19 @@
+import isStrongPassword from 'validator/lib/isStrongPassword'
+
+const validatePassword = (password: string) =>
+  isStrongPassword(password, {
+    minLength: 8,
+    minLowercase: 0,
+    minUppercase: 0,
+    minNumbers: 0,
+    minSymbols: 0,
+    returnScore: false,
+    pointsPerUnique: 1,
+    pointsPerRepeat: 0.5,
+    pointsForContainingLower: 10,
+    pointsForContainingUpper: 10,
+    pointsForContainingNumber: 10,
+    pointsForContainingSymbol: 10,
+  })
+
+export default validatePassword
