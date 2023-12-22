2d46a33642800af59e4ed5ca56fa061199a98adf
wwwhoami <subman450@gmail.com>
2023-12-22 18:50:47 +0300

    FormSubmit: add disabled attribute to submit button

--- src/components/form/FormSubmit.tsx
+++ src/components/form/FormSubmit.tsx
@@ -12,13 +12,16 @@ export default function FormSubmit({
   isSubmitting,
   children,
 }: Props) {
+  const disabled = !formIsValid || isSubmitting
+
   return (
     <button
       type="submit"
+      disabled={disabled}
       className={clsx(
         `inline-flex items-center focus-ring focus-within:ring-primary w-full justify-center rounded-lg bg-indigo-600 px-5 py-2.5 text-center font-medium text-sm text-white focus-within:ring focus-within:ring-opacity-50 hover:bg-indigo-500 focus:outline-none sm:w-auto`,
         {
-          'cursor-not-allowed opacity-80': !formIsValid || isSubmitting,
+          'cursor-not-allowed opacity-80': disabled,
         },
       )}
     >
