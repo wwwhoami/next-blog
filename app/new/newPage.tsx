95e998769d8d895682a389e7bfb85f0dacedf1d4
wwwhoami <subman450@gmail.com>
2024-01-01 03:14:04 +0300

    Add CategoryInput component to newPage.tsx

--- app/new/newPage.tsx
+++ app/new/newPage.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import PageHeading from '@/components/PageHeading'
+import CategoryInput from '@/components/category/CategoryInput'
 import { TextareaAutoExpand } from '@/components/form/TextareaAutoExpand'
 import withAuth from 'src/hocs/withAuth'
 
@@ -16,11 +17,8 @@ function NewPage({}: Props) {
         size="3xl"
         weight="semibold"
       />
-      <input
-        type="text"
-        placeholder="Add up to 4 category tags..."
-        className="w-full p-2 my-2 border border-gray-300 rounded-md dark:bg-gray-800 dark:border-gray-600 focus:outline-none focus:ring-1 focus:ring-blue-600"
-      />
+      <CategoryInput />
+
       <TextareaAutoExpand placeholder="Excerpt of your post..." rows={3} />
     </div>
   )
