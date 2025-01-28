18875e05b27e8a66f4416c471fc57ce20dd3b3c6
wwwhoami <subman450@gmail.com>
2025-01-29 00:26:34 +0300

    feat(icons): isolate svg icons into icon components
    - add Bars4CenterIcon, IndentIcon, and OutdentIcon components
    - add Booleanish type

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,5 @@
+import Bars4CenterIcon from './Bars4CenterIcon'
+import IndentIcon from './IndentIcon'
+import OutdentIcon from './OutdentIcon'
+
+export { Bars4CenterIcon, IndentIcon, OutdentIcon }
