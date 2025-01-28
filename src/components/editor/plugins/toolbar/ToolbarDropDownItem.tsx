1f647fb478863d4da018455159ccdc01cfa86fc2
wwwhoami <subman450@gmail.com>
2025-01-28 22:27:23 +0300

    fix(ToolbarDropDownItem): change props type && handle no icon

--- src/components/editor/plugins/toolbar/ToolbarDropDownItem.tsx
+++ src/components/editor/plugins/toolbar/ToolbarDropDownItem.tsx
@@ -2,8 +2,8 @@ import { MenuItem } from '@headlessui/react'
 import React from 'react'
 
 type Props = {
-  onClick: () => void
-  icon: React.ReactNode
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
+  icon?: React.ReactNode
   text: string
   keybinding?: string
 }
@@ -20,7 +20,7 @@ export default function ToolbarDropDownItem({
         className="group flex w-full items-center gap-2 rounded-lg px-3 py-1.5 data-[focus]:bg-gray-200 dark:data-[focus]:bg-white/10"
         onClick={onClick}
       >
-        <span className="size-4 opacity-50">{icon}</span>
+        {icon && <span className="size-4 opacity-50">{icon}</span>}
         {text}
         {keybinding && (
           <kbd className="ml-auto hidden font-sans text-xs group-data-[focus]:inline">
