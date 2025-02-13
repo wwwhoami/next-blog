2ae155258d2a6041a61f8e18f36f311bfc9cf6e6
wwwhoami <subman450@gmail.com>
2025-02-13 22:28:17 +0300

    style(editor.EditorTheme): add more padding to code components

--- src/components/editor/EditorTheme.ts
+++ src/components/editor/EditorTheme.ts
@@ -34,7 +34,7 @@ const editorTheme: EditorThemeClasses = {
     underlineStrikethrough: 'underline line-through',
     code: 'font-mono text-[94%] bg-gray-100 dark:bg-gray-600 dark:text-white p-1 rounded',
   },
-  code: 'bg-[#EEF1F6] text-ctp-text dark:bg-[#23273B] font-mono block py-2 px-8 leading-1 m-0 mt-2 mb-2 tab-2 overflow-x-auto relative before:absolute before:content-[attr(data-gutter)] before:bg-gray-200 dark:before:bg-gray-700 before:left-0 before:top-0 before:p-2 before:min-w-[25px] before:whitespace-pre-wrap before:text-right after:content-[attr(data-highlight-langrage)] after:right-3 after:absolute',
+  code: 'bg-[#EEF1F6] text-ctp-text dark:bg-[#23273B] font-mono block py-2 px-10 leading-1 m-0 mt-2 mb-2 tab-2 overflow-x-auto relative before:absolute before:content-[attr(data-gutter)] before:bg-gray-200 dark:before:bg-gray-700 before:left-0 before:top-0 before:p-2 before:min-w-[25px] before:whitespace-pre-wrap before:text-right after:content-[attr(data-highlight-langrage)] after:right-3 after:absolute',
   codeHighlight: {
     atrule: 'text-ctp-green',
     attr: 'text-ctp-green',
