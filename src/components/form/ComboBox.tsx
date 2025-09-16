e2c5d2268748f51470707b9e1466067b59b0212f
wwwhoami <subman450@gmail.com>
2025-09-16 04:52:22 +0300

    fix(editor): change combobox z-index to fix overflow with editor

--- src/components/form/ComboBox.tsx
+++ src/components/form/ComboBox.tsx
@@ -58,7 +58,7 @@ export default function MyCombobox({ categories, selected, handleAdd }: Props) {
             leaveTo="opacity-0"
             afterLeave={() => setQuery('')}
           >
-            <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
+            <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none sm:text-sm">
               {query !== '' && !isSelected(query) && (
                 <Combobox.Option
                   key={query}
