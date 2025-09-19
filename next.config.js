e73471069fa14f6a1d985bb13023d8a30c3dffb0
wwwhoami <subman450@gmail.com>
2025-09-20 00:34:49 +0300

    chore(next.config): add new hosts for allowed images

--- next.config.js
+++ next.config.js
@@ -2,23 +2,33 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [{
-      protocol: 'https',
-      hostname: 'randomuser.me'
-    },
-    {
-      protocol: 'https',
-      hostname: 'cloudflare-ipfs.com'
-    },
-    {
-      protocol: 'https',
-      hostname: 'loremflickr.com'
-    },
-    {
-      protocol: 'https',
-      hostname: 'avatars.githubusercontent.com'
-    }]
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cloudflare-ipfs.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'loremflickr.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.jsdelivr.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
